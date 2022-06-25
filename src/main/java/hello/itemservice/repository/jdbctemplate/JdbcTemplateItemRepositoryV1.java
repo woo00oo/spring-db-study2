7cedffab9a55bcc6d06c17e26d0dd960f0e4bdd5
woo00oo <96woo94@naver.com>
2022-06-26 01:25:24 +0900

    JdbcTemplate 적용3 - 구성과 실행

--- src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
+++ src/main/java/hello/itemservice/repository/jdbctemplate/JdbcTemplateItemRepositoryV1.java
@@ -63,7 +63,7 @@ public class JdbcTemplateItemRepositoryV1 implements ItemRepository {
 
     @Override
     public Optional<Item> findById(Long id) {
-        String sql = "select, id, item_name, price, quantity from item where id = ?";
+        String sql = "select id, item_name, price, quantity from item where id = ?";
         try {
             Item item = template.queryForObject(sql, itemRowMapper(), id); //단건 조회
             return Optional.of(item);
